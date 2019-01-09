a62fe3015580782fe0f804cd46217f2015e5e742
Aurélien Nicolas <aurel@qed-it.com>
2019-01-09 10:19:10 +0100

    Working request/response between Rust and C++

--- cpp/gadget.cpp
+++ cpp/gadget.cpp
@@ -1,33 +1,70 @@
-#include "gadget.capnp.h"
+#include <iostream>
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
-#include <iostream>
+#include "gadget.capnp.h"
+#include "gadget.h"
 
 using std::cout;
 using std::endl;
 
-#include "gadget.h"
 
 bool gadget_request(
-        char*                    request,
-        uint64_t                 request_len,
+        char *request_ptr,
+        uint64_t request_len,
         gadget_handle_response_t chunk_callback,
-        void*                    chunk_context,
+        void *chunk_context,
         gadget_handle_response_t response_callback,
-        void*                    response_context
+        void *response_context
 ) {
-    cout << "Got request " << request_len << endl;
 
-    if (chunk_callback != NULL) {
-        auto chunk = request;
-        auto chunk_len = request_len;
-        chunk_callback(chunk_context, chunk, chunk_len);
+    uint64_t freeVarId;
+    {
+        auto words = kj::ArrayPtr<capnp::word>((capnp::word *) request_ptr, request_len / sizeof(capnp::word));
+        auto message = capnp::FlatArrayMessageReader(words);
+        auto request = message.getRoot<AssignmentsRequest>();
+        auto instance = request.getInstance();
+        freeVarId = instance.getFreeVariableId();
+        cout << "Got request: len=" << request_len << " bytes, freeVariableId=" << freeVarId << endl;
     }
 
-    if (response_callback != NULL) {
-        auto response = request;
-        auto response_len = request_len;
-        response_callback(response_context, response, response_len);
+    {
+        capnp::MallocMessageBuilder message;
+
+        AssignmentsChunk::Builder chunk = message.initRoot<AssignmentsChunk>();
+        capnp::List<AssignedVariable>::Builder assignments = chunk.initAssignments(2);
+
+        AssignedVariable::Builder var0 = assignments[0];
+        var0.setVariableId(freeVarId);
+        capnp::Data::Builder val0 = var0.initValue(3);
+        val0[0] = 10;
+        val0[1] = 11;
+        val0[2] = 12;
+
+        AssignedVariable::Builder var1 = assignments[1];
+        var1.setVariableId(freeVarId + 1);
+        capnp::Data::Builder val1 = var1.initValue(3);
+        val1[0] = 8;
+        val1[1] = 7;
+        val1[2] = 6;
+
+        if (chunk_callback != NULL) {
+            auto words = capnp::messageToFlatArray(message);
+            auto bytes = words.asBytes();
+            chunk_callback(chunk_context, (char *) bytes.begin(), bytes.size());
+        }
+    }
+
+    {
+        capnp::MallocMessageBuilder message;
+
+        AssignmentsResponse::Builder response = message.initRoot<AssignmentsResponse>();
+        response.setFreeVariableId(freeVarId + 2);
+
+        if (response_callback != NULL) {
+            auto words = capnp::messageToFlatArray(message);
+            auto bytes = words.asBytes();
+            response_callback(response_context, (char *) bytes.begin(), bytes.size());
+        }
     }
 
     return true;
--- src/gadget_c_api.rs
+++ src/gadget_c_api.rs
@@ -3,67 +3,144 @@
 // @author Aurélien Nicolas <aurel@qed-it.com>
 // @date 2019
 
-extern crate libc;
-
+use std::error::Error;
 use std::path::PathBuf;
+use std::slice;
+use std::marker::PhantomData;
+use capnp;
+use capnp::{
+    serialize::{write_message, read_message, OwnedSegments},
+    message::{Builder, ReaderOptions, Allocator, Reader},
+    traits::{Owned, FromPointerReader},
+};
+use gadget_capnp::{assignments_request, assignments_chunk, assignments_response};
 
-
+#[allow(improper_ctypes)]
 extern "C" {
     fn gadget_request(
         request: *const u8,
         request_len: u64,
-
-        chunk_callback: extern fn(context_ptr: *mut AssignmentChunkContext, chunk: *const u8, chunk_len: u64) -> bool,
-        chunk_context: *mut AssignmentChunkContext,
-
-        response_callback: extern fn(context_ptr: *mut AssignmentChunkContext, response: *const u8, response_len: u64) -> bool,
-        response_context: *mut AssignmentChunkContext,
+        chunk_callback: extern fn(context_ptr: *mut AssignmentContext, chunk: *const u8, chunk_len: u64) -> bool,
+        chunk_context: *mut AssignmentContext,
+        response_callback: extern fn(context_ptr: *mut AssignmentContext, response: *const u8, response_len: u64) -> bool,
+        response_context: *mut AssignmentContext,
     ) -> bool;
 }
 
-#[repr(C)]
-pub struct AssignmentChunkContext {
-    call_count: u64,
+
+/// A message wrapper attached the expected reader type.
+pub struct Message<T>(Reader<OwnedSegments>, PhantomData<T>)
+    where T: for<'b> Owned<'b>;
+
+impl<T> Message<T> where T: for<'b> Owned<'b> {
+    pub fn reader(&self) -> Result<<T as Owned>::Reader, capnp::Error> {
+        self.0.get_root()
+    }
+}
+
+pub struct AssignmentContext {
+    chunks: Vec<Message<assignments_chunk::Owned>>,
+    response: Option<Message<assignments_response::Owned>>,
+}
+
+// Bring arguments from C calls back into the type system.
+fn from_c<'a, CTX>(
+    context_ptr: *mut CTX,
+    chunk: *const u8,
+    chunk_len: u64,
+) -> capnp::Result<(&'a mut CTX, Reader<OwnedSegments>)> {
+    let mut context = unsafe { &mut *context_ptr };
+    let buf = unsafe { slice::from_raw_parts(chunk, chunk_len as usize) };
+    let msg = read_message(&mut buf.clone(), ReaderOptions::new())?;
+    Ok((context, msg))
 }
 
 extern "C"
-fn assignment_chunk_callback(
-    context_ptr: *mut AssignmentChunkContext,
-    chunk: *const u8,
+fn assignment_chunk_callback_c(
+    context_ptr: *mut AssignmentContext,
+    chunk_ptr: *const u8,
     chunk_len: u64,
 ) -> bool {
-    let context = unsafe { &mut *context_ptr };
-    context.call_count += 1;
-    return true;
+    match from_c(context_ptr, chunk_ptr, chunk_len) {
+        Ok((context, msg)) => {
+            context.chunks.push(Message(msg, PhantomData));
+            true
+        }
+        Err(err) => {
+            println!("Error in assignment_chunk_callback: {}", err);
+            false
+        }
+    }
 }
 
+extern "C"
+fn assignment_response_callback_c(
+    context_ptr: *mut AssignmentContext,
+    chunk_ptr: *const u8,
+    chunk_len: u64,
+) -> bool {
+    match from_c(context_ptr, chunk_ptr, chunk_len) {
+        Ok((context, msg)) => {
+            context.response = Some(Message(msg, PhantomData));
+            true
+        }
+        Err(err) => {
+            println!("Error in assignment_response_callback: {}", err);
+            false
+        }
+    }
+}
 
-pub fn make_witness(gadget_name: &str) -> AssignmentChunkContext {
-    let request: &[u8] = &[];
+pub fn make_witness<A: Allocator>(message: &Builder<A>) -> Result<AssignmentContext, String> {
+    let mut request_buf: Vec<u8> = vec![];
+    write_message(&mut request_buf, &message);
 
-    let mut context = AssignmentChunkContext {
-        call_count: 0,
+    let mut context = AssignmentContext {
+        chunks: vec![],
+        response: None,
     };
 
-    unsafe {
-        assert!(gadget_request(
-            request.as_ptr(),
-            request.len() as u64,
-            assignment_chunk_callback,
-            &mut context as *mut _ as *mut AssignmentChunkContext,
-            assignment_chunk_callback,
-            &mut context as *mut _ as *mut AssignmentChunkContext,
-        ));
+    let request_ptr = request_buf.as_ptr();
+    let ok = unsafe {
+        gadget_request(
+            request_ptr,
+            request_buf.len() as u64,
+            assignment_chunk_callback_c,
+            &mut context as *mut _ as *mut AssignmentContext,
+            assignment_response_callback_c,
+            &mut context as *mut _ as *mut AssignmentContext,
+        )
+    };
+
+    match ok {
+        false => Err("gadget_request failed".to_string()),
+        true => Ok(context),
     }
-
-    println!("gadget_request called back {} times.", context.call_count);
-
-    return context;
 }
 
 #[test]
 fn test_gadget_request() {
-    unsafe {
-        make_witness("x");
+    let mut message = Builder::new_default();
+    {
+        let mut request = message.init_root::<assignments_request::Builder>();
+        let mut instance = request.init_instance();
+        instance.set_free_variable_id(100);
     }
+
+    let assign_ctx = unsafe {
+        make_witness(&message).unwrap()
+    };
+
+    println!("gadget_request sent {} chunks and {} response.", assign_ctx.chunks.len(), if assign_ctx.response.is_some() { "a" } else { "no" });
+
+    let chunk = assign_ctx.chunks[0].reader().unwrap();
+    let assignments = chunk.get_assignments().unwrap();
+    println!("Got {} assignments", assignments.len());
+    for a in assignments.iter() {
+        println!("{} = {:?}", a.get_variable_id(), a.get_value().unwrap());
+    }
+
+    let response = assign_ctx.response.unwrap();
+    let response = response.reader().unwrap();
+    println!("Next free variable id: {}", response.get_free_variable_id());
 }
