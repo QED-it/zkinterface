b4b8c9226fcc95cde0f23222a55b51e4ef6c0603
Aurélien Nicolas <aurel@qed-it.com>
2019-02-14 13:33:51 +0100

    Wrap inline documentation to fit the spec page.

--- cpp/gadget.h
+++ cpp/gadget.h
@@ -7,9 +7,11 @@ extern "C" {
 
 /*  Callback functions.
 
-    The caller implements these functions and passes function pointers to the gadget.
-    The caller may also pass pointers to arbitrary opaque `context` objects of its choice.
-    The gadget calls the callbacks with its response messages, and repeating the context pointer.
+    The caller implements these functions and passes function pointers
+    to the gadget. The caller may also pass pointers to arbitrary opaque
+    `context` objects of its choice.
+    The gadget calls the callbacks with its response messages,
+    and repeating the context pointer.
  */
 typedef bool (*gadget_callback_t)(
         void *context,
@@ -19,14 +21,18 @@ typedef bool (*gadget_callback_t)(
 /*  A function that implements a gadget.
 
     It receives a `GadgetCall` message, callbacks, and callback contexts.
-    It calls `constraints_callback` one or more times with `constraints_context` and a `R1CSConstraints` message.
-    It calls `assigned_variables_callback` one or more times with `assigned_variables_context` and a `AssignedVariables` message.
-    Finally, it calls `return_callback` with `return_context` and a `GadgetReturn` message.
+    It calls `constraints_callback` one or more times with
+    `constraints_context` and a `R1CSConstraints` message.
+    It calls `assigned_variables_callback` one or more times with
+    `assigned_variables_context` and a `AssignedVariables` message.
+    Finally, it calls `return_callback` with `return_context` and a
+    `GadgetReturn` message.
     The callbacks and the contexts pointers may be identical and may be NULL.
 
-    The following memory management convention is used both for `call_gadget` and for the callbacks.
-    All pointers passed as arguments to a function are only valid for the duration of this function call.
-    The code calling a function is responsible for managing the pointed objects after the function returns.
+    The following memory management convention is used both for `call_gadget`
+    and for the callbacks. All pointers passed as arguments to a function are
+    only valid for the duration of this function call. The caller of a function
+    is responsible for managing the pointed objects after the function returns.
 */
 bool call_gadget(
         unsigned char *call_msg,
