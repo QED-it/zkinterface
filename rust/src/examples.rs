066f540832eecf653174d3b0f06c85e638cfe6d8
Aurélien Nicolas <aurel@qed-it.com>
2019-05-08 22:10:07 +0200

    Improve and test example circuit

--- rust/src/examples.rs
+++ rust/src/examples.rs
@@ -1,18 +1,19 @@
-use writing::{CircuitOwned, VariablesOwned};
+use flatbuffers::{emplace_scalar, EndianScalar, FlatBufferBuilder};
 use std::io;
-use flatbuffers::FlatBufferBuilder;
+use std::mem::size_of;
+use writing::{CircuitOwned, VariablesOwned};
 use zkinterface_generated::zkinterface::{
-    Variables,
-    VariablesArgs,
     BilinearConstraint,
     BilinearConstraintArgs,
+    Message,
     R1CSConstraints,
     R1CSConstraintsArgs,
-    Witness,
-    WitnessArgs,
     Root,
     RootArgs,
-    Message,
+    Variables,
+    VariablesArgs,
+    Witness,
+    WitnessArgs,
 };
 
 
@@ -20,11 +21,12 @@ pub fn example_circuit() -> CircuitOwned {
     example_circuit_inputs(3, 4, 25)
 }
 
-pub fn example_circuit_inputs(x: u8, y: u8, zz: u8) -> CircuitOwned {
+/// A test circuit of inputs x,y,zz such that x^2 + y^2 = zz.
+pub fn example_circuit_inputs(x: u32, y: u32, zz: u32) -> CircuitOwned {
     CircuitOwned {
         connections: VariablesOwned {
             variable_ids: vec![1, 2, 3],  // x, y, zz
-            values: Some(vec![x, y, zz]), // x^2 + y^2 = zz
+            values: Some(serialize_small(&[x, y, zz])),
         },
         free_variable_id: 6,
         r1cs_generation: true,
@@ -85,9 +87,12 @@ pub fn write_example_witness<W: io::Write>(writer: W) -> io::Result<()> {
     write_example_witness_inputs(writer, 3, 4)
 }
 
-pub fn write_example_witness_inputs<W: io::Write>(mut writer: W, x: u8, y: u8) -> io::Result<()> {
-    let ids = vec![4, 5 as u64];           // xx, yy
-    let values = vec![x * x, y * y as u8]; // x^2, y^2
+pub fn write_example_witness_inputs<W: io::Write>(mut writer: W, x: u32, y: u32) -> io::Result<()> {
+    let ids = [4, 5 as u64]; // xx, yy
+    let values = serialize_small(&[
+        x * x, // var_4 = xx = x^2
+        y * y, // var_5 = yy = y^2
+    ]);
 
     let mut builder = &mut FlatBufferBuilder::new();
     let ids = builder.create_vector(&ids);
@@ -107,4 +112,31 @@ pub fn write_example_witness_inputs<W: io::Write>(mut writer: W, x: u8, y: u8) -
     builder.finish_size_prefixed(message, None);
 
     writer.write_all(builder.finished_data())
+}
+
+pub fn serialize_small<T: EndianScalar>(values: &[T]) -> Vec<u8> {
+    let sz = size_of::<T>();
+    let mut buf = vec![0u8; sz * values.len()];
+    for i in 0..values.len() {
+        emplace_scalar(&mut buf[sz * i..], values[i]);
+    }
+    buf
+}
+
+
+#[test]
+fn test_examples() {
+    use reading::Messages;
+
+    let mut buf = Vec::<u8>::new();
+    example_circuit().write(&mut buf).unwrap();
+    write_example_constraints(&mut buf).unwrap();
+    write_example_witness(&mut buf).unwrap();
+
+    let mut msg = Messages::new(1);
+    msg.push_message(buf).unwrap();
+    assert_eq!(msg.into_iter().count(), 3);
+    assert_eq!(msg.circuits().len(), 1);
+    assert_eq!(msg.iter_constraints().count(), 3);
+    assert_eq!(msg.iter_assignment().count(), 2);
 }
