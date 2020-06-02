b5494b2b80bf379f8becd6bf8075bbe49bf45946
aurel <aurel@qed-it.com>
2020-06-02 22:23:51 +0200

    Convenience VariablesOwned.get_variables()

--- rust/src/owned/variables.rs
+++ rust/src/owned/variables.rs
@@ -5,6 +5,7 @@ use zkinterface_generated::zkinterface::{
     Variables,
     VariablesArgs,
 };
+use reading::Variable;
 
 #[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize)]
 pub struct VariablesOwned {
@@ -21,13 +22,34 @@ impl<'a> From<Variables<'a>> for VariablesOwned {
                 Some(var_ids) => Vec::from(var_ids.safe_slice()),
                 None => vec![],
             },
-            values: variables_ref.values().map(|bytes|
-                Vec::from(bytes)),
+            values: match variables_ref.values() {
+                Some(bytes) => Some(Vec::from(bytes)),
+                None => None,
+            },
         }
     }
 }
 
 impl VariablesOwned {
+    pub fn get_variables(&self) -> Vec<Variable> {
+        let values = match self.values {
+            Some(ref values) => values as &[u8],
+            None => &[], // No values, only variable ids and empty values.
+        };
+
+        let n_vars = self.variable_ids.len();
+        let stride = values.len() / n_vars;
+
+        (0..n_vars)
+            .map(|var_i|
+                Variable {
+                    id: self.variable_ids[var_i],
+                    // Extract the value. Possibly empty.
+                    value: &values[stride * var_i..stride * (var_i + 1)],
+                }
+            ).collect()
+    }
+
     /// Add this structure into a Flatbuffers message builder.
     pub fn build<'bldr: 'args, 'args: 'mut_bldr, 'mut_bldr>(
         &'args self,
