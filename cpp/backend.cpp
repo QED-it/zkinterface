72c5aa36531b9b9428ae8312bcb144e9080d60fe
aurel <aurel@qed-it.com>
2020-06-09 12:44:44 +0200

    libsnark: copy constraints and witness into protoboard

--- cpp/backend.cpp
+++ cpp/backend.cpp
@@ -1,8 +1,8 @@
-
-#include "libsnark_zkif_import.cpp"
 #include <fstream>
 #include <iterator>
 #include <chrono>
+#include <libff/common/default_types/ec_pp.hpp>
+#include "libsnark_zkif_import.cpp"
 
 using namespace zkinterface_libsnark;
 
@@ -23,24 +23,34 @@ vector<char> read_file() {
 }
 
 void run() {
-    // TODO: init curves.
+    libff::alt_bn128_pp::init_public_params();
 
     auto buf = read_file();
 
     protoboard<FieldT> pb;
 
-    import_zkif iz;
+    import_zkif iz(pb, "import_zkif");
     iz.load(buf);
 
     auto begin = chrono::steady_clock::now();
 
-    iz.get_circuit();
+    iz.allocate_variables();
     iz.generate_constraints();
     iz.generate_witness();
+
+    cout << pb.get_constraint_system() << endl;
+    for(auto i=0;i<6;i++){
+        cout << i << ": " << pb.val(i).as_ulong() << endl;
+    }
 
     auto end = chrono::steady_clock::now();
     cout << "It took " << chrono::duration_cast<chrono::microseconds>(end - begin).count() << "µs"
          << endl;
+
+    cout << pb.num_inputs() << " public inputs" << endl;
+    cout << pb.num_variables() << " variables" << endl;
+    cout << pb.num_constraints() << " constraints" << endl;
+    cout << "Satisfied: " << (pb.is_satisfied() ? "YES" : "NO") << endl;
 }
 
 int main(int, char **) {
