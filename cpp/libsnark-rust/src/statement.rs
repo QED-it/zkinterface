3ec125b175026bdc2af98872a636c6085ffb4f65
aurel <aurel@qed-it.com>
2020-07-02 13:01:05 +0200

    libsnark-e2e: manage free_variable_id in builder. Error handling.

--- cpp/libsnark-rust/src/statement.rs
+++ cpp/libsnark-rust/src/statement.rs
@@ -1,6 +1,8 @@
 use std::io::{Read, Write};
-use std::error::Error;
 use std::fs::File;
+use std::error::Error;
+
+pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 use zkinterface::owned::{
     variables::VariablesOwned,
@@ -12,58 +14,77 @@ use zkinterface::owned::{
 use zkinterface::reading::Messages;
 use super::gadgetlib::call_gadget;
 
-struct StatementBuilder {
+pub struct StatementBuilder {
     pub out_path: String,
     pub constraints_file: File,
     pub witness_file: File,
     pub gadgets_file: File,
+
+    pub free_variable_id: u64,
 }
 
 impl StatementBuilder {
-    pub fn new(out_path: &str) -> StatementBuilder {
-        StatementBuilder {
+    pub fn new(out_path: &str) -> Result<StatementBuilder> {
+        Ok(StatementBuilder {
             out_path: out_path.to_string(),
-            constraints_file: File::create(format!("{}_constraints.zkif", out_path)).unwrap(),
-            witness_file: File::create(format!("{}_witness.zkif", out_path)).unwrap(),
-            gadgets_file: File::create(format!("{}_gadgets.zkif", out_path)).unwrap(),
-        }
+            constraints_file: File::create(format!("{}_constraints.zkif", out_path))?,
+            witness_file: File::create(format!("{}_witness.zkif", out_path))?,
+            gadgets_file: File::create(format!("{}_gadgets.zkif", out_path))?,
+            free_variable_id: 1,
+        })
     }
 
-    pub fn call_gadget(&mut self, circuit: &CircuitOwned, command: &CommandOwned) -> Result<Messages, Box<dyn Error>> {
-        circuit.write(&mut self.gadgets_file).unwrap();
+    pub fn call_gadget(&mut self, circuit: &CircuitOwned, command: &CommandOwned) -> Result<Messages> {
+        circuit.write(&mut self.gadgets_file)?;
 
         let (constraints, witness, response) = call_gadget(circuit, command)?;
 
+        let free_variable_id = response.last_circuit().ok_or("no response")?.free_variable_id();
+        assert!(free_variable_id >= self.free_variable_id);
+        self.free_variable_id = free_variable_id;
+
         for msg in &constraints.messages {
-            self.constraints_file.write_all(msg).unwrap();
+            self.constraints_file.write_all(msg)?;
         }
         for msg in &witness.messages {
-            self.witness_file.write_all(msg).unwrap();
+            self.witness_file.write_all(msg)?;
         }
         for msg in &response.messages {
-            self.gadgets_file.write_all(msg).unwrap();
+            self.gadgets_file.write_all(msg)?;
         }
 
         Ok(response)
     }
 
-    pub fn push_witness(&mut self, witness: &WitnessOwned) {
-        witness.write(&mut self.witness_file).unwrap();
+    pub fn push_witness(&mut self, witness: &WitnessOwned) -> Result<()> {
+        Ok(witness.write(&mut self.witness_file)?)
     }
 
-    pub fn write_main(&self, statement: &CircuitOwned) {
+    pub fn write_main(&self, statement: &CircuitOwned) -> Result<()> {
         let main_path = format!("{}_main.zkif", self.out_path);
-        let mut file = File::create(&main_path).unwrap();
-        statement.write(&mut file).unwrap();
+        let mut file = File::create(&main_path)?;
+        Ok(statement.write(&mut file)?)
+    }
+
+    pub fn allocate(&mut self) -> u64 {
+        let id = self.free_variable_id;
+        self.free_variable_id += 1;
+        id
+    }
+
+    pub fn allocate_many(&mut self, n: usize) -> Vec<u64> {
+        let first_id = self.free_variable_id;
+        self.free_variable_id += n as u64;
+        (first_id..self.free_variable_id).collect()
     }
 }
 
 
 #[test]
-fn test_statement() {
-    fn main(b: &mut StatementBuilder, proving: bool) {
+fn test_statement() -> Result<()> {
+    fn main(b: &mut StatementBuilder, proving: bool) -> Result<()> {
         let some_vars = VariablesOwned {
-            variable_ids: vec![1, 2, 3, 4],
+            variable_ids: b.allocate_many(4),
             values: if proving {
                 Some(vec![11, 12, 9, 14 as u8])
             } else {
@@ -72,13 +93,13 @@ fn test_statement() {
         };
 
         if proving {
-            b.push_witness(&WitnessOwned { assigned_variables: some_vars.clone() });
+            b.push_witness(&WitnessOwned { assigned_variables: some_vars.clone() })?;
         }
 
         let gadget_res = {
             let gadget_call = CircuitOwned {
                 connections: some_vars.clone(),
-                free_variable_id: some_vars.variable_ids.len() as u64 + 1,
+                free_variable_id: b.free_variable_id,
                 field_maximum: None,
                 configuration: Some(vec![
                     KeyValueOwned {
@@ -89,7 +110,7 @@ fn test_statement() {
                     }]),
             };
             let command = CommandOwned { constraints_generation: true, witness_generation: proving };
-            b.call_gadget(&gadget_call, &command).unwrap()
+            b.call_gadget(&gadget_call, &command)?
         };
 
         let statement = CircuitOwned {
@@ -97,7 +118,7 @@ fn test_statement() {
                 variable_ids: vec![],
                 values: if proving { Some(vec![]) } else { None },
             },
-            free_variable_id: gadget_res.last_circuit().unwrap().free_variable_id(),
+            free_variable_id: b.free_variable_id,
             field_maximum: None,
             configuration: Some(vec![
                 KeyValueOwned {
@@ -107,20 +128,20 @@ fn test_statement() {
                     number: 0,
                 }]),
         };
-        b.write_main(&statement);
+        b.write_main(&statement)
     }
 
     {
         let out_path = "local/test_statement_verifier";
-        let mut b = StatementBuilder::new(out_path);
-        main(&mut b, false);
+        let mut b = StatementBuilder::new(out_path)?;
+        main(&mut b, false)?;
         println!("Writen {}*.zkif", out_path);
     }
 
     {
         let out_path = "local/test_statement_prover";
-        let mut b = StatementBuilder::new(out_path);
-        main(&mut b, true);
+        let mut b = StatementBuilder::new(out_path)?;
+        main(&mut b, true)?;
         println!("Writen {}*.zkif", out_path);
     }
 
@@ -151,4 +172,6 @@ fn test_statement() {
     assert_eq!(prover_main.connections.values, Some(vec![]));
     prover_main.connections.values = None;
     assert_eq!(prover_main, verifier_main);
+
+    Ok(())
 }
