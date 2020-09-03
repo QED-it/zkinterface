462a29d7c1dcaa63acb195e8debfa6e9dadf4bc9
aurel <aurel@qed-it.com>
2020-09-03 15:36:51 +0200

    gates: pretty constant

--- rust/src/gates/mod.rs
+++ rust/src/gates/mod.rs
@@ -5,12 +5,38 @@ use gatesystem::GatesSystemOwned;
 use gates::GateOwned::*;
 use std::io::Write;
 use crate::Result;
+use hex;
+
+
+pub fn pretty_print(gatesystem: &GatesSystemOwned) {
+    for gate in &gatesystem.gates {
+        match gate {
+            Constant(output, constant) =>
+                eprintln!("wire_{:?} = new constant 0x{}", output, hex::encode(constant)),
+
+            InstanceVar(output) =>
+                eprintln!("wire_{:?} = new instance", output),
+
+            Witness(output) =>
+                eprintln!("wire_{:?} = new witness", output),
+
+            AssertZero(input) =>
+                eprintln!("assert wire_{:?} == 0", input),
+
+            Add2(output, left, right) =>
+                eprintln!("wire_{:?} = wire_{:?} + wire_{:?}", output, left, right),
+
+            Mul2(output, left, right) =>
+                eprintln!("wire_{:?} = wire_{:?} * wire_{:?}", output, left, right),
+        }
+    }
+}
 
 
 pub fn write_example_gate_system(writer: &mut impl Write) -> Result<()> {
     let sys = GatesSystemOwned {
         gates: vec![
-            Constant(1, vec![11]),
+            Constant(1, vec![0x11]),
             InstanceVar(2),
             Witness(3),
             Mul2(4, 1, 2),
@@ -31,28 +57,9 @@ fn test_gate_system() -> Result<()> {
     let gate_system = get_size_prefixed_root_as_root(&buf).message_as_gates_system().unwrap();
 
     let owned = GatesSystemOwned::from(gate_system);
+
     eprintln!("{:?}\n", owned);
+    pretty_print(&owned);
 
-    for gate in &owned.gates {
-        match gate {
-            Constant(output, constant) =>
-                eprintln!("wire_{:?} = {:?}", output, constant),
-
-            InstanceVar(output) =>
-                eprintln!("wire_{:?} = new instance", output),
-
-            Witness(output) =>
-                eprintln!("wire_{:?} = new witness", output),
-
-            AssertZero(input) =>
-                eprintln!("assert wire_{:?} == 0", input),
-
-            Add2(output, left, right) =>
-                eprintln!("wire_{:?} = wire_{:?} + wire_{:?}", output, left, right),
-
-            Mul2(output, left, right) =>
-                eprintln!("wire_{:?} = wire_{:?} * wire_{:?}", output, left, right),
-        }
-    }
     Ok(())
 }
