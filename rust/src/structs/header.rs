612736ec0f6f10766f948dd01a3ed668e0e99a94
Lef Ioannidis <elefthei@seas.upenn.edu>
2021-07-16 13:29:27 -0400

    Write the zkif header

--- rust/src/structs/header.rs
+++ rust/src/structs/header.rs
@@ -140,7 +140,7 @@ impl CircuitHeader {
     pub fn write_into(&self, writer: &mut impl Write) -> Result<()> {
         let mut builder = FlatBufferBuilder::new();
         let message = self.build(&mut builder);
-        builder.finish_size_prefixed(message, None);
+        fb::finish_size_prefixed_root_buffer(&mut builder, message);
         writer.write_all(builder.finished_data())?;
         Ok(())
     }
